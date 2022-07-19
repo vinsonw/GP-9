e40ca29629b8cf3345e9623b57cb22c2e60452c0
vinsonw <me@vinsonwei.com>
2022-07-19 11:48:30 +0800

    Add prototypical inheritance

---
+++ code_with_mosh/JS/Part2-04-Prototypical-Inheritance.js
@@ -0,0 +1,19 @@
+// Prototypical Inheritance
+
+
+// Reset constructor function
+
+
+// Call the super constructor
+
+
+// Intermediate Function Inheritance
+
+// extend(Child, Parent)
+
+
+// Method Overriding
+
+
+// Polymorphism 
+
